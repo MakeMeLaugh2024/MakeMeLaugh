15690f2353860da9fdfc2f701becc219cfe992d1
kekekuli <kekekuliobj@gmail.com>
2024-01-27 11:18:31 +0800

    使用rigidbody2D.velocity进行移动、跳跃

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))] 
 public class Player : MonoBehaviour
 {
-    private PlayerControls controls;
-    private Vector2 movement;
-
     [ControlScheme("LeftKeyboard", "RightKeyboard")]
     [SerializeField] string controlScheme;
+    [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float jumpForce = 10.0f;
+    [SerializeField] Vector2 checkSize = new Vector2(0.5f, 0.2f);  // 检测的大小，你可以根据需要调整
+    [SerializeField] Vector2 checkPositionOffset = Vector2.down * 0.5f;  // 检测的位置偏移，你可以根据需要调整
+
+    private PlayerControls controls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
 
     private void Awake() {
         controls = new PlayerControls();
         controls.bindingMask = InputBinding.MaskByGroup(controlScheme);
         controls.Enable();
-
         controls.Player.Jump.performed += ctx => OnJump();
+
+        rb = GetComponent<Rigidbody2D>();    
     }
 
     private void Update() {
         PlayerInput(); 
     }
     private void FixedUpdate() {
-         
+        Move();   
     }
-
     private void PlayerInput() {
         movement = controls.Player.Move.ReadValue<Vector2>();
     }
-
     public void OnJump() {
-        Debug.Log(gameObject + ":Jump");
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void Move() {
+        Vector2 newVelocity = movement * moveSpeed;
+        newVelocity.y = rb.velocity.y;  // 保持原来的垂直速度，以便受到重力的影响
+        rb.velocity = newVelocity;
+    }
+
+    public bool IsGrounded() {
+        LayerMask groundLayer = LayerMask.GetMask("Ground");  // 地面的层，你需要在Unity中设置
+
+        // 使用OverlapBox进行覆盖检测
+        bool isGrounded = Physics2D.OverlapBox((Vector2)transform.position + checkPositionOffset, checkSize, 0, groundLayer);
+
+        return isGrounded;
+    }
+    void OnDrawGizmos() {
+        Vector2 checkPosition = (Vector2)transform.position + checkPositionOffset;
+
+        // 设置Gizmos的颜色
+        Gizmos.color = Color.red;
+
+        // 绘制一个矩形
+        Gizmos.DrawWireCube(checkPosition, checkSize);
     }
 }
